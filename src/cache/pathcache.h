1c1fd298251923eb8afa6235bf3e5f271abd2b71
Marcus <marcusposey@gmail.com>
2018-02-27 17:43:59 -0600

    Document PathCache

--- src/cache/pathcache.h
+++ src/cache/pathcache.h
@@ -8,15 +8,31 @@
 
 namespace beta {
 
+/**
+ * Holds a collection of queryable paths in memory
+ *
+ * PathCache contains a collection of paths that are optimally shareable. I.e., given
+ * a node origin and destination, one should hope to retrieve a path between them
+ * that is based on a master cached path.
+ */
 class PathCache {
 public:
+    /**
+     * Constructs a PathCache instance
+     * @param entries The collection to start the cache with. This variable will
+     *                be modified by the constructor.
+     * @param capacityBytes The maximum size of the cache during its lifetime
+     */
     PathCache(std::vector<cache_entry> &entries, size_t capacityBytes);
+
+    /** Adds an entry to the cache */
     void add(const cache_entry &entry);
+    /** Retrieves a path that answers the given query or nullptr if no such path exists */
     std::shared_ptr<Path> find(const path_query &query) const;
     
-    // TODO: void save(std::shared_ptr<CacheSaver> saver);
-
+    /** Returns the maximum allowed size (in bytes) of the cache */
     size_t capacity() const { return capacityBytes; }
+    /** Returns the current size (in bytes) of the cache */
     size_t size() const { return cache.size(); }
 
 private:
