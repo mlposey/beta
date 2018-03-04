3d4326fbf6811487cfc8af30aad1aaad4b489903
Marcus <marcusposey@gmail.com>
2018-03-03 20:23:40 -0600

    Add help related methods to FlagParser

--- src/flag/flagparser.h
+++ src/flag/flagparser.h
@@ -5,8 +5,10 @@ namespace beta {
 
 /** Describes a type that parses command line flags */
 struct FlagParser {
-    virtual ~FlagParser() = delete;
+    virtual ~FlagParser() = default;
     virtual void parse(int argc, char **argv) = 0;
+    virtual bool wantsHelp() const = 0;
+    virtual void showHelp() const = 0;
     virtual std::string provider() const = 0;
 };
 
