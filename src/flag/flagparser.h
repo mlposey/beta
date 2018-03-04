17986839919c98a7a114ac829feab74fb321b994
Marcus <marcusposey@gmail.com>
2018-03-03 20:00:10 -0600

    Create interface for flag parsing

---
+++ src/flag/flagparser.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+namespace beta {
+
+/** Describes a type that parses command line flags */
+struct FlagParser {
+    virtual ~FlagParser() = delete;
+    virtual void parse(int argc, char **argv) = 0;
+    virtual std::string provider() const = 0;
+};
+
+}
