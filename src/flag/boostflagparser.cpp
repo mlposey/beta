81885d592c62c353001022f6a2dff6a94f3781bc
Marcus <marcusposey@gmail.com>
2018-03-03 21:02:57 -0600

    Add more details to the provider usage string

--- src/flag/boostflagparser.cpp
+++ src/flag/boostflagparser.cpp
@@ -7,7 +7,11 @@ BoostFlagParser::BoostFlagParser()
     : flagsDefinition("options") {
     flagsDefinition.add_options()
         ("help", "show usage information")
-        ("provider", po::value<std::string>(), "specify provider (e.g., gmaps)");
+        ("provider", po::value<std::string>(), "optionally specify a provider\n"
+                                               "This should not include the file extension.\n"
+                                               "E.g, use gmaps -- not gmaps.so\n"
+                                               "To change the provider search path, alter\n"
+                                               "the provider_dir setting in config.json.");
 }
 
 void BoostFlagParser::parse(int argc, char **argv) {
