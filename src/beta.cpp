c1f928c5c855926dbb5439031ec3c0c47aeef7d0
Marcus <marcusposey@gmail.com>
2018-03-03 20:55:50 -0600

    Parse provider from command-line flags

--- src/beta.cpp
+++ src/beta.cpp
@@ -1,19 +1,44 @@
 #include "config/config.h"
 #include "cache/pathcache.h"
+#include "provider/providerloader.h"
 #include "net/server.h"
+#include "flag/boostflagparser.h"
 #include "version.h"
 
 using namespace beta;
 
-int main() {
+/** Loads a provider or defaults to nullptr if it cannot be found or was not requested */
+std::shared_ptr<PathProvider> loadProvider(FlagParser *flagParser);
+
+int main(int argc, char **argv) {
     printf("BeTA v%s\n", APP_VERSION);
+
+    BoostFlagParser flagParser;
+    flagParser.parse(argc, argv);
+    if (flagParser.wantsHelp()) {
+        flagParser.showHelp();
+        return 0;
+    }
+
     Config &config = Config::getInstance();
-
-    // TODO: Load entries from disk if needed.
     PathCache cache(config.getBytes("max_cache_mem"));
-
-    // TODO: Load provider if needed.
-    Server server(&cache, nullptr);
+    
+    Server server(&cache, loadProvider(&flagParser));
     server.listen(config.getString("port"));
     return 0;
+}
+
+std::shared_ptr<PathProvider> loadProvider(FlagParser *flagParser) {
+    std::shared_ptr<PathProvider> provider = nullptr;
+    try {
+        ProviderLoader loader(Config::getInstance().getString("provider_dir"));
+        std::string providerName = flagParser->provider();
+        if (!providerName.empty()) {
+            provider = loader.load(providerName + ".so");
+        }
+    } catch (std::exception &e) {
+        std::cout << e.what() << '\n'
+                  << "all path retrieval will default to GetRoute behavior\n";
+    }
+    return provider;
 }
